bcd0e8c06612c2bbbc4006ca4c6eaf7beb7338af
pranta-barua007 <prantabarua247@gmail.com>
2021-12-28 15:55:19 +0600

    updates

--- api.js
+++ api.js
@@ -5,13 +5,10 @@ const cors = require('cors');
 
 const api = express();
 
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-api.use(helmet());
-api.use(cors(corsOptions));
+api.use(helmet({
+    contentSecurityPolicy: false,
+}));
+api.use(cors());
 
 api.use(express.static(path.join(__dirname, 'public')));
 
